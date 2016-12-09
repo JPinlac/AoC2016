dc01e7190b1eaaf31c9fdbf5d63811769d8854bc
Jonathan <jonppinlac@gmail.com>
2016-12-09 10:23:40 -0500

    Final refactor, reused code for both parts

--- AoC-day3.playground/Contents.swift
+++ AoC-day3.playground/Contents.swift
@@ -3,33 +3,13 @@
 import UIKit
 
 guard let triangles = try? String(contentsOf: #fileLiteral(resourceName: "input.txt")) else { fatalError() }
-//let triangles = "  810  679   10\n783  255  616\n545  626  626\n   84  910  149\n607  425  901\n556  616  883"
-var triangleCount = 0
-triangles.enumerateLines { (line, _) in
-    let singleItemArray = line.components(separatedBy: CharacterSet.newlines)[0].components(separatedBy: CharacterSet.whitespaces)
-        .filter{
-            $0 != ""
-        }
-        .map{ item in
-            Int(item)
-        }
-        .flatMap{
-            $0
-        }
-        .sorted(by: { (s1: Int, s2:Int) -> Bool in
-            s1 < s2
-    })
-    if singleItemArray[0] + singleItemArray[1] > singleItemArray[2] {
-        triangleCount += 1
-    }
-}
-print(triangleCount)
 
+var triangleCount = 0
 var triangleCount2 = 0
 var part2TriangleArray = [[Int]](arrayLiteral: [],[],[])
 var row = 0
-triangles.enumerateLines { (line, _) in
-    let singleItemArray = line.components(separatedBy: CharacterSet.newlines)[0].components(separatedBy: CharacterSet.whitespaces)
+triangles.enumerateLines { (line, hi) in
+    var singleItemArray = line.components(separatedBy: CharacterSet.whitespaces)
         .filter{
             $0 != ""
         }
@@ -38,7 +18,17 @@ triangles.enumerateLines { (line, _) in
         }
         .flatMap{
             $0
+        }
+    
+// Part 1 Solution
+    let sortedArray1 = singleItemArray.sorted(by: { (v1: Int, v2:Int) -> Bool in
+        v1 < v2
+    })
+    
+    if sortedArray1[0] + sortedArray1[1] > sortedArray1[2] {
+        triangleCount += 1
     }
+// Part 2 Solution
     if row < 3 {
         for i in 0...2 {
             part2TriangleArray[i].append(singleItemArray[i])
@@ -48,18 +38,17 @@ triangles.enumerateLines { (line, _) in
     if row == 3 {
         row = 0
         part2TriangleArray.forEach { item in
-            let sortedArray = item.sorted(by: { (s1: Int, s2:Int) -> Bool in
+            let sortedArray2 = item.sorted(by: { (s1: Int, s2:Int) -> Bool in
                 s1 < s2
             })
-            if sortedArray[0] + sortedArray[1] > sortedArray[2] {
+            if sortedArray2[0] + sortedArray2[1] > sortedArray2[2] {
                 triangleCount2 += 1
             }
         }
         part2TriangleArray = [[Int]](arrayLiteral: [],[],[])
     }
-
 }
-
+print(triangleCount)
 print(triangleCount2)
 
 
